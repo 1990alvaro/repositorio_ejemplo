e35cb7026733676e707f6d61ff78e6f71535a842
alvaro1990 <1990alvaro.aragon@gmail.com>
2022-08-26 12:37:38 -0500

    v3-productos

--- src/producto.java
+++ src/producto.java
@@ -1,3 +1,12 @@
 public class producto {
+    private String nombre;
+    private String precio;
+    private String categoria;
+
+    public producto(String nombre, String precio, String categoria) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.categoria = categoria;
+    }
     
 }
