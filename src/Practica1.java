0cde79330fbcc711b53e425f6658a1524b7c0f95
alvaro1990 <1990alvaro.aragon@gmail.com>
2022-08-26 11:30:35 -0500

    version inicial

---
+++ src/Practica1.java
@@ -0,0 +1,11 @@
+public class Practica1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+       System.out.println("probando repositorio");
+    }
+    
+}
